b0ae90eb8aa93acee56567549740975f63db2cda
vankhang49 <dvkhang49@gmail.com>
2024-10-29 08:27:53 +0700

    vankhang49

--- src/main/java/com/example/apartmentmanagement/repository/permission/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.apartmentmanagement.repository.permission;
-
-import com.example.apartmentmanagement.entities.permission.AppRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<AppRole, Long> {
-    AppRole findByRoleName(String roleName);
-}
--- src/main/java/com/example/apartmentmanagement/service/permission/IRoleService.java
+++
@@ -1,8 +0,0 @@
-package com.example.apartmentmanagement.service.permission;
-
-import com.example.apartmentmanagement.entities.permission.AppRole;
-import com.example.apartmentmanagement.service.IGeneralService;
-
-public interface IRoleService extends IGeneralService<AppRole> {
-    AppRole findRoleByRoleName(String roleName);
-}
